6ec9761a15f09a52742b27076f16ba5e531f99a8
Francisco Souza <f@souza.cc>
2012-03-09 20:58:24 -0300

    tagging: subseting django-tagging
    all my tags are separated by commas, i don't need the other code ;)

--- tagging/utils.py
+++ tagging/utils.py
@@ -28,60 +28,7 @@ def parse_tag_input(input):
 
     input = force_unicode(input)
 
-    # Special case - if there are no commas or double quotes in the
-    # input, we don't *do* a recall... I mean, we know we only need to
-    # split on spaces.
-    if u',' not in input and u'"' not in input:
-        words = list(set(split_strip(input, u' ')))
-        words.sort()
-        return words
-
-    words = []
-    buffer = []
-    # Defer splitting of non-quoted sections until we know if there are
-    # any unquoted commas.
-    to_be_split = []
-    saw_loose_comma = False
-    open_quote = False
-    i = iter(input)
-    try:
-        while 1:
-            c = i.next()
-            if c == u'"':
-                if buffer:
-                    to_be_split.append(u''.join(buffer))
-                    buffer = []
-                # Find the matching quote
-                open_quote = True
-                c = i.next()
-                while c != u'"':
-                    buffer.append(c)
-                    c = i.next()
-                if buffer:
-                    word = u''.join(buffer).strip()
-                    if word:
-                        words.append(word)
-                    buffer = []
-                open_quote = False
-            else:
-                if not saw_loose_comma and c == u',':
-                    saw_loose_comma = True
-                buffer.append(c)
-    except StopIteration:
-        # If we were parsing an open quote which was never closed treat
-        # the buffer as unquoted.
-        if buffer:
-            if open_quote and u',' in buffer:
-                saw_loose_comma = True
-            to_be_split.append(u''.join(buffer))
-    if to_be_split:
-        if saw_loose_comma:
-            delimiter = u','
-        else:
-            delimiter = u' '
-        for chunk in to_be_split:
-            words.extend(split_strip(chunk, delimiter))
-    words = list(set(words))
+    words = list(set(split_strip(input)))
     words.sort()
     return words
 
@@ -109,22 +56,7 @@ def edit_string_for_tags(tags):
     resulting string of tag names will be comma-delimited, otherwise
     it will be space-delimited.
     """
-    names = []
-    use_commas = False
-    for tag in tags:
-        name = tag.name
-        if u',' in name:
-            names.append('"%s"' % name)
-            continue
-        elif u' ' in name:
-            if not use_commas:
-                use_commas = True
-        names.append(name)
-    if use_commas:
-        glue = u', '
-    else:
-        glue = u' '
-    return glue.join(names)
+    return ", ".join([t.name for t in tags])
 
 def get_queryset_and_model(queryset_or_model):
     """
